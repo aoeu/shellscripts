00c7d33f6e3b4575bc28151277aa4c8d8210144c
Travis snɯǝᗡɔW <aoeu@users.noreply.github.com>
2019-10-11 19:59:27 -0400

    Handle final newline removal for acme

--- fmtdoc.sh
+++ fmtdoc.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-test "$*" = "" && fmt -p '//' -w 80 || echo "$*" | fmt -p '//' -w 80
+function format {
+	fmt -p '//' -w 80
+}
+function removeFinalNewlineForAcme {
+	perl -p -e 'chomp if eof'
+}
+test "$*" = "" &&  format | removeFinalNewlineForAcme || echo "$*" | format
