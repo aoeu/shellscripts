6c250e0402274209287fded0a3a74973a34ad5ce
Travis snɯǝᗡɔW <aoeu@users.noreply.github.com>
2020-09-29 22:58:00 -0400

    Kludge the awful difference of BSD 'readlink -f' differing from GNU 'readlink -f'

--- fishlogin.bash
+++ fishlogin.bash
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+linkread() {
+	/usr/bin/env python -c 'import os; import sys; print os.path.realpath(sys.argv[1])' $1
+}
+
 s='/usr/local/bin/fishlogin'
 test $# -eq 1 && test "$1" = "-i" && \
 	echo "installing to $s then adding to /etc/shells then running chsh, all with sudo" && \
 	[[ "$(read -e -p 'OK? [y/N]> '; echo $REPLY)" == [Yy]* ]] && \
-	sudo cp $(readlink -f $0) "$s" && \
+	sudo cp $(linkread $0) "$s" && \
 	sudo sh -c "echo ${s} >> /etc/shells" && \
 	sudo chsh -s "$s" $USER
 
