398b803ff93784609a5102f4552ba81e1b83e9d5
Travis snɯǝᗡɔW <aoeu@users.noreply.github.com>
2020-09-29 22:56:30 -0400

    Replace chown hack for making sudo work with redirect

--- fishlogin.bash
+++ fishlogin.bash
@@ -5,9 +5,7 @@ test $# -eq 1 && test "$1" = "-i" && \
 	echo "installing to $s then adding to /etc/shells then running chsh, all with sudo" && \
 	[[ "$(read -e -p 'OK? [y/N]> '; echo $REPLY)" == [Yy]* ]] && \
 	sudo cp $(readlink -f $0) "$s" && \
-	sudo chown $USER:root /etc/shells && \
-	sudo echo "$s" >> /etc/shells && \
-	sudo chown root:root /etc/shells && \
+	sudo sh -c "echo ${s} >> /etc/shells" && \
 	sudo chsh -s "$s" $USER
 
 exec /usr/bin/env fish $*
