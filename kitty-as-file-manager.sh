a55026ee7d759f2cf085d22bc46f8be08404e2ed
Travis snɯǝᗡɔW <aoeu@users.noreply.github.com>
2025-12-12 21:25:03 -0500

    Compensate for anti-social file naming

--- kitty-as-file-manager.sh
+++ kitty-as-file-manager.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # Some programs provide an encoded file URI as an arg,
 # others provide a filepath with an extra leading slash.
-/sbin/kitty --directory "$(/sbin/sed -e "s|^file:/||" -e 's|^//|/|' -e "s/+/ /g; s/%20/ /g" <<< "$1")"
+invisibleSpace="$(printf '\u200b')"
+d="$(/sbin/sed -e 's|^file:/||' -e 's|^//|/|' -e 's/%20/ /g; s/%5B/[/g; s/%5D/]/g;' -e "s/\%E2\%80\%8B/${invisibleSpace}/g" <<< "$1")"
+/sbin/kitty --directory "$d"
