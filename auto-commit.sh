07449301d4e891019dc6ba5eee6cdadc09bd202b
Travis snɯǝᗡɔW <aoeu@users.noreply.github.com>
2026-01-05 23:09:01 -0500

    Add some clean-up to auto-commit
    - Tighten-up quoting
    - Note limitation about moving files out of watched repository
    - Exclude self from auto-commit by name (instead of file extension)

--- auto-commit.sh
+++ auto-commit.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
-# debug protip: 26,$s/git/GIT_TRACE=1 git/g		26,$s/GIT_TRACE=1 //g
+# debug protip: 33,$s/git/GIT_TRACE=1 git/g		33,$s/GIT_TRACE=1 //g
+# Known limitations: A subsequent MOVED_TO event does not occur after a MOVED_FROM if
+# a file is moved (with mv) out of the watched repository, which this script cannot
+# realisitically account for, so simply don't do that (and cp with a subsquent rm instead).
 set -o errexit
 test ! -d '.git' && {
 	echo 2>&1 'not a git repository'
 	exit 1
+}
+pid=$(pgrep "${0#./}") && grep --invert-match --quiet "$$" <<< "$pid" && {
+        echo 2>&1 "an instance of '${0#./}' is already running"
+        exit 1 # TODO(aoeu): support running one instance per repo, not per system
 }
 printf \
 'Until stopped, this script will silently and automatically git commit
@@ -12,14 +19,14 @@ filesystem both in and beneath the current directory.
 
 We good with that? (y/N)\n'
 read -p	'> ' response
-(test "y" = "$response" && echo '🖏  Ctrl+c to exit') || exit
-message=""
+(test 'y' = "$response" && echo '🖏  Press Ctrl+c to exit.') || exit
+message=''
 inotifywait \
 	--quiet \
 	--monitor \
 	--recursive \
 	--event create,delete,move,close_write \
-	--exclude '\.git/|\.swp$|~$|\.tmp$|\.sh$|(^|/)[0-9]+$' \
+	--exclude "$0|\./\.git|"'\.git/|\.swp$|~$|\.tmp$|(^|/)[0-9]+$' \
 	--format '%e %w%f' \
 	'.' \
 | while read event filepath
@@ -27,7 +34,7 @@ do
 	case $event in
 	*,IS_DIR) continue ;;
 	CREATE|CLOSE_WRITE)
-		test -f "$filepath" && git add "$filepath" && message="change of"
+		test -f "$filepath" && git add "$filepath" && message='change of'
 	;;
 	MOVED_FROM)
 		test ! -f "$filepath" && git rm --quiet "$filepath" && {
@@ -39,12 +46,12 @@ do
 		test -f "$filepath" && git add "$filepath"
 	;;
 	DELETE)
-		test ! -f "$filepath" && git rm --quiet "$filepath" && message="deletion of" 
+		test ! -f "$filepath" && git rm --quiet "$filepath" && message='deletion of'
 	;;
 	esac
 	errorStatusIfIndexDirty='git diff-index --quiet --cached HEAD'
 	test -z "$message" || eval "$errorStatusIfIndexDirty" || {
 		git commit --quiet --message "Auto-commit $message '${filepath#./}'"
-		message=""
+		message=''
 	}
 done
