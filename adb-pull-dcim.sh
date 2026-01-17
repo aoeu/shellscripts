9e85ee8e058f75bf3145aa9f8011636193bea5d4
Travis snɯǝᗡɔW <aoeu@users.noreply.github.com>
2026-01-16 20:27:37 -0500

    adb pull photos from DCIM with optionally specifiable date

--- adb-pull-dcim.sh
+++ adb-pull-dcim.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-adb shell "find /sdcard/DCIM/Camera -name '*_$(date +%Y%m%d)_*.jpg'" \
+when=$(echo "$*" | sed -E -e 's/\s+//; s/-//;')
+test -z "$when" && when=$(date +%Y%m%d)
+test ! "$when" -eq "$when" && test ! "${#when}" -eq 8 && {
+	echo 1>&2 "a date in format YYYYMMDD is required as an argument (received '$when')"
+	exit 1
+}
+adb shell "find /sdcard/DCIM/Camera -name '*_${when}_*.jpg'" \
+| grep -v '\.trashed' \
 | while read f; do adb pull "$f"; done
